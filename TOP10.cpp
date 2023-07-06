11b7ec75c63c1800001b908877c6a4e4f66246de
Apurv  Malani <123534851+ApurvMalani@users.noreply.github.com>
2023-07-06 22:19:09 +0530

    Add files via upload

---
+++ TOP10.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+
+        int x;
+        cin >> x;
+
+        if (x > 10)
+        {
+            printf("NO");
+        }
+        else
+        {
+            printf("Yes");
+        }
+        // pri new num all of  5 .. 4 ,15 ,10 ,1 ,50 .. lIke
+
+        cout << "\n";
+    }
+
+    return 0;
+}
